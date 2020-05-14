c82b5bddb3a5b5d2cbf13aea08acbb524c8f0996
applepie3141 <59668828+applepie3141@users.noreply.github.com>
2020-05-14 09:07:43 -0700

    Fixed
    Fixed the broken code earlier, fixed threeInRow(), added isFull()

--- TicTacToe.py
+++ TicTacToe.py
@@ -29,18 +29,24 @@ def printGrid(): #prints the current grid in a nice, readable fashion. Only work
 def threeInRow(): #checks to see if someone has won the game
     for x in range(0,7,3): #checks for rows
         if currentGrid[x]==currentGrid[x+1] and currentGrid[x+1]==currentGrid[x+2] and (currentGrid[x]=='X' or currentGrid[x]=="O"):
-            print('row')
-            return True, currentGrid[x]
+            print('Three in a row! ' + currentGrid[x] + ' won the game.')
+            return True
     for x in range(3):
         if currentGrid[x]==currentGrid[x+3] and currentGrid[x+3]==currentGrid[x+6] and (currentGrid[x]=='X' or currentGrid[x]=="O"):
-            print('column')
-            return True, currentGrid[x]
+            print('Three in a column! ' + currentGrid[x] + ' won the game.')
+            return True
     if ((currentGrid[0]==currentGrid[4] and currentGrid[4]==currentGrid[8]) or (currentGrid[2]==currentGrid[4] and currentGrid[4]==currentGrid[6])) and (currentGrid[4]=='X' or currentGrid[4]=="O"):
-        print('diag')
-        return True, currentGrid[4]
+        print('Three in a diagonal! ' + currentGrid[4] + ' won the game.')
+        return True
     else:
-        print('false')
         return False
+
+def isFull(): #checks to see if the grid has any empty squares left
+    for x in currentGrid:
+        if x != "X" and x != "O":
+            return False
+    else:
+        return True
 
 # Game startup
 print('Hello, welcome to Tic-Tac-Toe! Select your difficulty.')
@@ -48,11 +54,12 @@ difficulty = input("Valid inputs: EASY, MEDIUM, or HARD ")
 print(difficulty + " selected. Would you like to choose X's or O's? X goes first.")
 order = input('Valid inputs: X or O ')
 
+
 def plot():
     print('Where would you like to place your '+order+'?')
     xCoord = int(input('Input x-coordinate: '))
     yCoord = int(input('Input y-coordinate: '))
-    if currentGrid[((2-yCoord)*3)+xCoord] == '':
+    if currentGrid[((2-yCoord)*3)+xCoord] != 'X' and currentGrid[((2-yCoord)*3)+xCoord] != 'O':
         currentGrid[((2-yCoord)*3)+xCoord] = order
     else:
         print('Oops! That spot is already occupied.')
@@ -64,13 +71,10 @@ currentGrid = [
     '','','']
 printGrid()
 print('Remember to input your points as if it were a coordinate place.\ni.e. 0,0 is in the bottom left and 2,2 is the top right.')
-#threeInRow()
-while threeInRow() == False:# and '' in currentGrid:
+while not threeInRow() and not isFull():
     plot()
     printGrid()
 
 
-if threeInRow() == True:
-    print('Three in a row! '+threeInRow()[1] + ' has won the game.')
-else:
-    print("Cat's game! The round has ended in a draw.")
+if isFull():
+    print("Cat's game! The game ended in a tie.")
